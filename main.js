8dd096ffcb417b7f8568d7ca0e9c97e299bc90a6
benji <tbkertelt@outlook.com>
2023-11-14 12:19:05 -0300

    Added index.html, main.css, main.js

---
+++ main.js
@@ -0,0 +1,45 @@
+const form = document.getElementById('form-contato')
+
+const contato = [];
+const tel = [];
+
+let linhas = '';
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+    
+});
+
+function adicionaLinha() {
+    const inputNome = document.getElementById('nome')
+    const inputTelefone = document.getElementById('telefone')
+
+    if (contato.includes(inputNome.value)) {
+        alert(`O contato: ${inputNome.value} já foi adicionado`)
+    } else {
+
+        contato.push(inputNome.value);
+        tel.push(parseFloat(inputTelefone.value));
+
+        let linha = '<tr>';
+        linha += `<td>${inputNome.value}</td>`;
+        linha += `<td>${inputTelefone.value}</td>`;
+        linha += '</tr>';
+
+        linhas += linha;
+    }
+
+    inputNome.value = '';
+    inputTelefone.value = '';
+
+}
+
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+
+}
